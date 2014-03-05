9c4565a0d7235c02629d7b7cbaaa996000947d7f
mcslclehman <mcslc.lehman@gmail.com>
2014-03-05 12:56:39 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+  public static void main(String[] args) {
+    for (int i = 0, j = 1, k = 2; i < 5; i++){
+      System.out.println("I : " + i + ",j : " + j + ", k : " + k);
+    }
+  }
+}
+/*
+I : 0,j : 1, k : 2
+I : 1,j : 1, k : 2
+I : 2,j : 1, k : 2
+I : 3,j : 1, k : 2
+I : 4,j : 1, k : 2
+*/
