91e23620073ec78d80588f50cf9ac9ae819373e2
mcslclehman <mcslc.lehman@gmail.com>
2014-03-05 13:08:23 -0500

    Create ClassShapeMain.java

---
+++ ClassShapeMain.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) Ian F. Darwin, http://www.darwinsys.com/, 1996-2002.
+ * All rights reserved. Software written by Ian F. Darwin and others.
+ * $Id: LICENSE,v 1.8 2004/02/09 03:33:38 ian Exp $
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * Java, the Duke mascot, and all variants of Sun's Java "steaming coffee
+ * cup" logo are trademarks of Sun Microsystems. Sun's, and James Gosling's,
+ * pioneering role in inventing and promulgating (and standardizing) the Java 
+ * language and environment is gratefully acknowledged.
+ * 
+ * The pioneering role of Dennis Ritchie and Bjarne Stroustrup, of AT&T, for
+ * inventing predecessor languages C and C++ is also gratefully acknowledged.
+ */
+import java.util.*;
+
+/** Part of a main program using Shape objects */
+public class ClassShapeMain {
+
+  Collection allShapes;  // created in a Constructor, not shown
+
+  /** Iterate over all the Shapes, getting their areas */
+  public double totalAreas() {
+    Iterator it = allShapes.iterator();
+    double total = 0.0;
+    while (it.hasNext()) {
+      Shape s = (Shape)it.next();
+      total += s.computeArea();
+    }
+    return total;
+  }
+}
+class Circle extends Shape {
+  double radius;
+  public double computeArea() {
+    return Math.PI * radius * radius;
+  }
+}
+class Rectangle extends Shape {
+  double width, height;
+  public double computeArea() {
+    return width * height;
+  }
+}
+abstract class Shape {
+  protected int x, y;
+  public abstract double computeArea();
+}
+
